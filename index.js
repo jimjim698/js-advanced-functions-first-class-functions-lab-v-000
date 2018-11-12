b27d3cd8662ad4451b748b83c5576e8d19c72547
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-12 01:00:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ const returnLastTwoDrivers = function (drivers){
   return drivers.slice(-2)
 }
 
-const selectingDrivers = [function (){returnFirstTwoDrivers},returnLastTwoDrivers()]
+const selectingDrivers = [function returnFirstTwoDrivers,returnLastTwoDrivers]
 
-const h = [1,2,3]
